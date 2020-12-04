a6861338404b95cb00dcaa5f99a357b8b1ebf2fc
wb-ls374356@antfin.com <wb-ls374356@antfin.com>
2020-12-05 00:54:23 +0800

    fix: 文件上传接口案例

--- src/app.js
+++ src/app.js
@@ -1,30 +1,45 @@
 const express = require('express');
+const shelljs = require('shelljs');
+const path = require('path');
+const allowCors = require('./allowCors');
+
+const multipart = require('connect-multiparty');
+const multipartMiddleware = multipart();
 
 // 创建一个 app 实例
 const app = express();
-app.use(express.urlencoded())
-var bodyParser=require('body-parser')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:false}))
 
+// 允许跨域
+app.use(allowCors);
 
-app.get('/home/:id?', (req, res) => {
-  console.log(req);
+/** 
+ * @method 上传文件接口
+ * @param {File} file
+ * @param {any} param
+ */
+app.post('/upload', multipartMiddleware, (req, res) => {
+  if (
+    !req.files 
+      || !req.files.file 
+      || !req.files.file.path 
+      || !req.files.file.originalFilename
+      || !req.body.param
+  ) {
+    return res.json({
+      msg: '入参缺失'
+    })
+  }
 
-  console.log('========get query', req.query);
-  console.log('========get params', req.params);
-  console.log('========get body', req.body);
-  res.json(req.query)
+  shelljs.mv(
+    req.files.file.path, 
+    path.resolve(__dirname, `../public/${req.files.file.originalFilename}`)
+  );
+
+  res.json({
+    param: req.body.param,
+    msg: '文件上传成功',
+  })
 })
-
-app.post('/home/:id?', (req, res) => {
-  console.log('========post query', req.query);
-  console.log('========post body', req.body);
-  console.log('========post params', req.params);
-
-  res.json(req.body)
-})
-
 
 app.listen(3000, () => {
   console.log('启动成功，监听 3000端口')
